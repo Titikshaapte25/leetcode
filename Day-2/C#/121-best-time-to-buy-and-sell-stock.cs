665c65155dad739d7e19eb81372622ebaad8735d
Titiksha Apte <tapte@csod.com>
2024-01-31 13:57:43 +0530

    Updating the code for Q-121.

--- Day-2/C#/121-best-time-to-buy-and-sell-stock.cs
+++ Day-2/C#/121-best-time-to-buy-and-sell-stock.cs
@@ -1,3 +1,14 @@
+using System;				
+public class Program
+{
+	public static void Main()
+	{
+		Solution s = new Solution();
+		int result = s.MaxProfit(new int[] {1,2,4,2,5,7,2,4,9,0,9});
+		Console.WriteLine(result);	//result will be 9
+	}
+}
+
 public class Solution {
     public int MaxProfit(int[] prices) {
         int j=1, largestProfit =0,profit =0;
